81b24de2f1f7b67ac80ae72b31aeca68c5955b28
kyungsnim <kyungsnim@kakao.com>
2020-01-03 23:16:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,  // 상태바 색상 조절을 위해 import
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView,
+  AsyncStorage
+} from "react-native";
+import Movie from './Movie';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movie />
+      <MoviePoster />
+
+      {/* // #6 볼 차례입니다 */}
     </div>
   );
 }
-
+ 
+class MoviePoster extends Component {
+  render() {
+    return(
+      <img src="https://namu.wiki/w/%ED%97%9D%EA%B1%B0%20%EA%B2%8C%EC%9E%84:%20%EB%8D%94%20%ED%8C%8C%EC%9D%B4%EB%84%90" />
+    );
+  }
+}
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+export default class Movie extends Component {
+    render() {
+        return(
+            Hello
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
