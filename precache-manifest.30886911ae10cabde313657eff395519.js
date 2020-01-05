fcabe13eb61e4238e6331abbf0b19bc79d36bd86
kyungsnim <kyungsnim@kakao.com>
2020-01-05 19:19:57 +0900

    Updates

---
+++ precache-manifest.30886911ae10cabde313657eff395519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9deb4bd1fc0ef75163dcac3054a609b5",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "698cffd9ff55810ead1f",
+    "url": "/movie_app/static/css/main.6b10e32a.chunk.css"
+  },
+  {
+    "revision": "02c803fac3999cf1665c",
+    "url": "/movie_app/static/js/2.2b9d6ecc.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app/static/js/2.2b9d6ecc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "698cffd9ff55810ead1f",
+    "url": "/movie_app/static/js/main.d277dedc.chunk.js"
+  },
+  {
+    "revision": "b03db121bc0725d553bd",
+    "url": "/movie_app/static/js/runtime-main.4970d62d.js"
+  }
+]);
